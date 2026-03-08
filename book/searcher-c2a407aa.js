c8dc5ef9c1ef351a2855541601a0e1b5917cee6e
Vanilla-Beauty <Vanilla-Beauty@users.noreply.github.com>
2026-03-08 15:44:38 +0000

    deploy: 03bb10aff975d2dada7841e77ea4fc777a0e22b3

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-98cfd9c4.js',
+                path_to_root + 'searchindex-8f36f4e4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
